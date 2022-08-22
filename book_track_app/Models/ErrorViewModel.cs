108789b47b8cc734e5a3ca562b3d14c7691cb4ad
preetyabhishek <theshivamsharma01@gmail.com>
2022-08-22 13:26:45 -0400

    first commit

---
+++ book_track_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace book_track_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
